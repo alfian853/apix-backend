aa734da18ff7c70f6c5f462afc21de0d03f7a90e
Unknown <alfian853@gmail.com>
2019-02-04 12:39:38 +0700

    add api repository

--- repository/src/main/java/com/future/apix/repository/ApiRepository.java
+++ repository/src/main/java/com/future/apix/repository/ApiRepository.java
@@ -1,4 +1,13 @@
 package com.future.apix.repository;
 
-public class ApiRepository {
+import com.future.apix.entity.ApiProject;
+import com.future.apix.entity.ApiSection;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
+public interface ApiRepository extends MongoRepository<ApiProject, String>{
+
+
 }

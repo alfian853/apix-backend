e44a2d178172a3ca2af605d087d94d3742adeb35
alfian <alfian853@gmail.com>
2019-02-24 11:56:32 +0700

    add @field

--- entity/src/main/java/com/future/apix/entity/apidetail/Schema.java
+++ entity/src/main/java/com/future/apix/entity/apidetail/Schema.java
@@ -26,6 +26,7 @@ public class Schema {
 
     //untuk custom dataType, isi schema = isi custom dataType
     @JsonProperty("$ref")
+    @Field("$ref")
     String ref;
 
     /** if datatype = Object

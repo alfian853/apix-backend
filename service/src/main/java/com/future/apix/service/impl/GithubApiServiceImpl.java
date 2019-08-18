d05858888589f8f493e585c94e22b430464ca73f
natashaval <natasha.valentina1998@gmail.com>
2019-08-18 23:49:20 +0700

    github api filelist only file

--- service/src/main/java/com/future/apix/service/impl/GithubApiServiceImpl.java
+++ service/src/main/java/com/future/apix/service/impl/GithubApiServiceImpl.java
@@ -98,7 +98,7 @@ public class GithubApiServiceImpl implements GithubApiService {
         List<GHTreeEntry> treeEntries = gitHub.get().getRepository(repoName).getTree(branchName).getTree();
         List<String> fileList = new ArrayList<>();
         for (GHTreeEntry entry : treeEntries) {
-            fileList.add(entry.getPath());
+            if(entry.getType() == "blob") fileList.add(entry.getPath());
         }
         return fileList;
     }
